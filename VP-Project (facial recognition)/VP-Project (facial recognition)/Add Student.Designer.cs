03dc7bfcb943e858c7dc3bcd9669f5d6b117b01f
Ali Raza <alir9996@gmail.com>
2019-12-27 01:50:31 +0500

    Reccognizing

--- VP-Project (facial recognition)/VP-Project (facial recognition)/Add Student.Designer.cs
+++ VP-Project (facial recognition)/VP-Project (facial recognition)/Add Student.Designer.cs
@@ -29,27 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnSaveFace = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.lblEnterName = new System.Windows.Forms.Label();
+            this.ttxtBoxName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button2
+            // btnSaveFace
             // 
-            this.button2.Location = new System.Drawing.Point(87, 208);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 64);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Delete";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnSaveFace.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFace.Location = new System.Drawing.Point(119, 357);
+            this.btnSaveFace.Name = "btnSaveFace";
+            this.btnSaveFace.Size = new System.Drawing.Size(147, 39);
+            this.btnSaveFace.TabIndex = 5;
+            this.btnSaveFace.Text = "Save";
+            this.btnSaveFace.UseVisualStyleBackColor = true;
+            this.btnSaveFace.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(87, 93);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(121, 85);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(170, 64);
+            this.button1.Size = new System.Drawing.Size(147, 39);
             this.button1.TabIndex = 4;
             this.button1.Text = "Scan";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,11 +64,48 @@
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(385, 50);
+            this.imageBox1.Location = new System.Drawing.Point(318, 12);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(357, 243);
+            this.imageBox1.Size = new System.Drawing.Size(390, 250);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
+            // 
+            // lblEnterName
+            // 
+            this.lblEnterName.AutoSize = true;
+            this.lblEnterName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterName.Location = new System.Drawing.Point(43, 171);
+            this.lblEnterName.Name = "lblEnterName";
+            this.lblEnterName.Size = new System.Drawing.Size(55, 20);
+            this.lblEnterName.TabIndex = 6;
+            this.lblEnterName.Text = "Name";
+            this.lblEnterName.Click += new System.EventHandler(this.lblEnterName_Click);
+            // 
+            // ttxtBoxName
+            // 
+            this.ttxtBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ttxtBoxName.Location = new System.Drawing.Point(121, 171);
+            this.ttxtBoxName.Name = "ttxtBoxName";
+            this.ttxtBoxName.Size = new System.Drawing.Size(145, 26);
+            this.ttxtBoxName.TabIndex = 7;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(118, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 20);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(318, 280);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 116);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // Add_Student
             // 
@@ -69,21 +113,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
             this.ClientSize = new System.Drawing.Size(824, 421);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.ttxtBoxName);
+            this.Controls.Add(this.lblEnterName);
             this.Controls.Add(this.imageBox1);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSaveFace);
             this.Controls.Add(this.button1);
             this.Name = "Add_Student";
             this.Text = "Add_Student";
             this.Load += new System.EventHandler(this.Add_Student_Load);
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSaveFace;
         private System.Windows.Forms.Button button1;
         private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label lblEnterName;
+        private System.Windows.Forms.TextBox ttxtBoxName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- VP-Project (facial recognition)/VP-Project (facial recognition)/Add Student.cs
+++ VP-Project (facial recognition)/VP-Project (facial recognition)/Add Student.cs
@@ -11,23 +11,24 @@ using Emgu.CV;
 using Emgu.Util;
 using System.IO;
 using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
 namespace VP_Project__facial_recognition_
 {
     public partial class Add_Student : Form
     {
         MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
         HaarCascade faceDetected;
-        Image<Bgr,Byte> Frame;
+        Image<Bgr, Byte> Frame;
         Capture camera;
         //var image = new Image<Bgr, Byte>(new Bitmap(pictureBox1.Image));
         Image<Gray, byte> result;
         Image<Gray, byte> trainedFace = null;
         Image<Gray, byte> grayface = null;
-        List<Image<Gray, byte>> trainingImages =new  List<Image<Gray, byte>>();
-        List <string> labels =new List<string>();
-        List<string> Users= new List<string>();
-        int count, NumLabels,t;
-        string name, names=null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int count = 0, NumLabels, t=0;
+        string name, names = null;
         public Add_Student()
         {
             InitializeComponent();
@@ -35,23 +36,24 @@ namespace VP_Project__facial_recognition_
 
         private void Add_Student_Load(object sender, EventArgs e)
         {
-           
+
+            //faceDetected = new HaarCascade("haarcascade_frontalface_alt.xml");
             faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
             try
             {
                 string labelsInf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
                 string[] Labels = labelsInf.Split(',');
+
                 NumLabels = Convert.ToInt32(Labels[0]);
                 count = NumLabels;
+                
                 string faceload;
+                
                 for (int i = 1; i < NumLabels + 1; i++)
                 {
                     faceload = "face" + i + ".bmp";
                     trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "Faces/Faces.txt"));
                     labels.Add(Labels[i]);
-
-
-
                 }
             }
             catch (Exception ex)
@@ -62,7 +64,7 @@ namespace VP_Project__facial_recognition_
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (camera==null)
+            if (camera == null)
             {
                 try
                 {
@@ -73,46 +75,125 @@ namespace VP_Project__facial_recognition_
                     MessageBox.Show(exp.Message);
                 }
             }
-            if (camera!=null)
+            if (camera != null)
             {
                 camera.QueryFrame();
-                Application.Idle+=new EventHandler(FrameProcedure);
+                Application.Idle += new EventHandler(FrameProcedure);
             }
-               
+
         }
 
-private void FrameProcedure(object sender, EventArgs e)
-{
-    Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-    grayface = Frame.Convert<Gray, Byte>();
-    MCvAvgComp[][] facesDetectedNow = grayface.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
-    //frame.Draw(font.rect, new Bgr(Color.Green), 3);
-    foreach (MCvAvgComp f in facesDetectedNow[0])
-    {
-        result=Frame.Copy(f.rect).Convert<Gray,Byte>().Resize(100,100,Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-        Frame.Draw(f.rect, new Bgr(Color.Blue),3);
-        if (trainingImages.ToArray().Length!=0)
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayface = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayface.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+           //frame.Draw(font.rect, new Bgr(Color.Green), 3);
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                t++;
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Blue), 3);
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(count, 0.01);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+                //Users[t - 1] = name;
+                Users.Add("");
+            }
+            imageBox1.Image = Frame;
+            //names = "";
+            //Users.Clear();
+            t = 0;
+            //Names concatenation of persons recognized
+            for (int nnn = 0; nnn < facesDetectedNow[0].Length; nnn++)
+            {
+                names = names + Users[nnn] + ", ";
+            }
+
+            //Show the faces procesed and recognized
+            imageBox1.Image = Frame;
+            lblName.Text = names;
+            names = "";
+            Users.Clear();
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
-            Frame.Draw(name,ref font,new Point(f.rect.X-2,f.rect.Y-2), new Bgr(Color.Red));
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ttxtBoxName.Text == "" || ttxtBoxName.Text.Length < 2 || ttxtBoxName.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter name of person");
+            }
+            else
+            {
+                count += 1;
+                grayface = camera.QueryGrayFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+                MCvAvgComp[][] DetectedFace = grayface.DetectHaarCascade(faceDetected, 1.2, 10,
+                    HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+                foreach (MCvAvgComp f in DetectedFace[0])
+                {
+
+                    trainedFace = Frame.Copy(f.rect).Convert<Gray, Byte>();
+                    break;
+                }
+
+                trainedFace = result.Resize(100, 100, INTER.CV_INTER_CUBIC);
+
+                trainingImages.Add(trainedFace);
+               // pictureBox1.Image = trainedFace;
+
+                labels.Add(ttxtBoxName.Text);
+
+                File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+                }
+
+                MessageBox.Show("Face Stored.");
+                ttxtBoxName.Text = "";
+                ttxtBoxName.Focus();
+            }
+        }
+            //count = count + 1;
+            //grayface = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            //MCvAvgComp[][] detectedFaces = grayface.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            //foreach (MCvAvgComp f in detectedFaces[0])
+            //{
+            //    trainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+            //    break;
+            //}
+            //trainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            //trainingImages.Add(trainedFace);
+            //labels.Add(ttxtBoxName.Text);
+            //File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            //for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+            //{
+            //    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/Faces" + i + ".bmp");
+            //    File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+            //}
+            //MessageBox.Show(ttxtBoxName + " Added Successfully");
+        //}
+
+        private void lblEnterName_Click(object sender, EventArgs e)
+        {
 
         }
-    }
-    imageBox1.Image = Frame;
-    
-}
 
-private void pictureBox1_Click(object sender, EventArgs e)
-{
-
-}
-
-private void button2_Click(object sender, EventArgs e)
-{
-    Frame.Save("C:\ali.jpg");
-    MessageBox.Show("Saved successfully");
-}
-      
     }
 }
